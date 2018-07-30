728c2c94d4ca415f7c7ca86739955876f4a28d57
707768680 <707768680@qq.com>
2018-07-30 18:26:09 +0800

    1

--- README.md
+++ README.md
@@ -10,8 +10,7 @@
 1. 完成 Header 区域, 使用的是 Mint-UI 中的 Header 组件
 2. 制作底部 Tabbar 区域, 使用的是 MUI 的 Tabbar.html
 3. 在中间区域放置一个 router-view 来展示路由匹配到的组件
- + css样式拷贝
- + tff文件拷贝
+ + css样式拷贝  tff文件拷贝
  + 修改图标的样式
 
 ## 改造tabbar 为 router-link
@@ -25,3 +24,4 @@
 2.使用 vue-resource 的 this.$http.get 获取数据
 3.获取到的数据,要保存到 data 身上
 4.使用 v-for 循环渲染每个 item 项
+5......
--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
 // 入口文件
-import Vue from 'vue'
+import Vue from "vue";
 
 // 导入 app 组件
-import app from './App.vue'
+import app from "./App.vue";
 
 // 导入 MUI 样式
-import './lib/mui/css/mui.css'
+import "./lib/mui/css/mui.css";
 
 // 导入扩展图标
-import './lib/mui/css/icons-extra.css'
+import "./lib/mui/css/icons-extra.css";
 
 // 导入路由的包
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";  
 // 安装路由
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 导入自己的router.js路由模块
-import router from './router.js'
+import router from "./router.js";
 
 // 按需导入 Mint-UI 中的组件
-import { Header,Swipe, SwipeItem } from 'mint-ui'
-Vue.component(Header.name, Header) 
+import { Header, Swipe, SwipeItem } from "mint-ui";
+Vue.component(Header.name, Header);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 
 // 导入 vue-resource
-import VueResource from 'vue-resource' 
+import VueResource from "vue-resource";
 // 安装 vue-resource
-Vue.use(VueResource)
+Vue.use(VueResource);
 
 var vm = new Vue({
-	el:'#app',
-	render: c => c(app),
-	router // 挂载路由到实例
-})
+  el: "#app",
+  render: c => c(app),
+  router // 挂载路由到实例
+});
