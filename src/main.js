97933500d3adc6b95cd409b50fd31bbc051ed8ce
707768680 <707768680@qq.com>
2018-07-30 17:26:31 +0800

    aaa

--- src/main.js
+++ src/main.js
@@ -7,12 +7,30 @@ import app from './App.vue'
 // 导入 MUI 样式
 import './lib/mui/css/mui.css'
 
-// 按需导入 Mint-UI 中的组件
-import { Header } from 'mint-ui'
-Vue.component(Header.name, Header) 
+// 导入扩展图标
+import './lib/mui/css/icons-extra.css'
 
+// 导入路由的包
+import VueRouter from 'vue-router'
+// 安装路由
+Vue.use(VueRouter)
+
+// 导入自己的router.js路由模块
+import router from './router.js'
+
+// 按需导入 Mint-UI 中的组件
+import { Header,Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header) 
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+// 导入 vue-resource
+import VueResource from 'vue-resource' 
+// 安装 vue-resource
+Vue.use(VueResource)
 
 var vm = new Vue({
 	el:'#app',
-	render: c => c(app)
+	render: c => c(app),
+	router // 挂载路由到实例
 })
